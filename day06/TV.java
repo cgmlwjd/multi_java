6fd50ab43405bdfb13345036e8a8ae5cc7dd0fa5
Choe huijeong <chj06190306@gmail.com>
2023-09-09 00:03:38 +0900

    10 commit 수정

--- day06/TV.java
+++ day06/TV.java
@@ -1,29 +1,32 @@
 package day06;
-//has a : 속성 2개 ~ 3개
-//메서드 3개
+//has a : 속성 2개 ~3개
+//메서드 : 3개
 public class TV {
-	String color = "black"; //인스턴스 변수(non-static변수) 객체명, 변수
-	int size;
-	int channel;
+	
+	String color="black";//인스턴스 변수(non-static변수)	객체명.변수
+	int size;//0
+	int channel;//0
 	boolean state;
 	
-	static String wifi = "KT";
+	static String wifi="KT";//클래스 변수(static변수)  클래스명.변수
+	//static==> 망치 ==> 객체들마다 공유한다
 	
-	public void changeChannel(int ch) { //인스턴스 메서드(non-static 메서드) 객체명, 메서드명()
-		channel = ch;
+	public void changeChannel(int ch) { //인스턴스 메서드(non-static메서드)  객체명.메서드명()
+		channel=ch;
 	}
 	public void powerOn() {
-		state = true;
+		state=true;
 	}
-	
 	public void powerOff() {
-		state = false;
+		state=false;
 	}
 	
 	public String info() {
-		String str = "--- TV정보 ---\n";
-		str += "크기 " + size + "인치\n";
-		str += "색상 " + color + "\n";
+		
+		String str="---TV정보----\n";
+		str+="크기: "+size+"인치\n";
+		str+="색상: "+color+"\n";
+		str+="WIFI: "+wifi+"\n";
 		return str;
 	}
 }
